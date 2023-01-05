3dcaacb7e45b9a8b876dd15f3c738fd969efd09f
faizamahmoud <faiza.mahmoud@gmail.com>
2023-01-05 09:54:20 -0700

    created home page

---
+++ my-app/pages/events/event1.tsx
@@ -0,0 +1,9 @@
+// localhost:3000/events/event1
+
+const Page = () => {
+    return (
+      <h1>Event Page</h1>
+    )
+  }
+  
+  export default Page;
---
+++ my-app/pages/events/index.tsx
@@ -0,0 +1,10 @@
+
+// localhost:3000/events
+
+const Page = () => {
+    return (
+      <h1>Event Folder</h1>
+    )
+  }
+  
+  export default Page;
